87dcc074c0da705fd8966098893c0f9126a93a60
alekusa <alekusa@gmail.com>
2022-12-07 11:34:59 -0300

    Add Cors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import postsRouters from "./routes/posts.routes.js";
 import fileUpload from "express-fileupload";
+import cors from "cors";
 
 const app = express();
+//uso de cors permite get de distintas url
+app.use(cors());
 //middleware
 app.use(express.json());
 //subida de archivos
