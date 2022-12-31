37b0c47d4623bdb68d9d338aa8f2bd60562ec019
AleKusa <alekusa@gmail.com>
2022-12-31 16:00:32 -0300

    add movie controler deleted/update/findOne

--- server/controllers/movie.controller.js
+++ server/controllers/movie.controller.js
@@ -1,5 +1,5 @@
 import Movie from '../models/movie.model.js'
-
+//crear una
 export const createMovie = async (req, res) => {
     try {
         //await Movie.sync({ force: true })
@@ -11,7 +11,7 @@ export const createMovie = async (req, res) => {
         return res.status(500).json({ message: error.message })
     }
 }
-
+//mostrar todas
 export const getMovies = async (req, res) => {
     try {
         const movies = await Movie.findAll()
@@ -19,4 +19,41 @@ export const getMovies = async (req, res) => {
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
+}
+//editar una
+export const editMovie = async (req, res) => {
+    const { id } = req.params
+    const { picture, title, calification } = req.body
+    try {
+        await Movie.update(
+            { picture, title, calification },
+            {
+                where: {
+                    id
+                }
+            }
+        )
+        const edit = await Movie.findByPk(id)
+        res.json(edit)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+//mostrar una
+export const getMovie = async (req, res) => {
+    const { id } = req.params
+    try {
+        const movie = await Movie.findByPk(id)
+        res.json(movie)
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+}
+//eliminar una
+export const deleteMovie = async (req, res) => {
+    const { id } = req.params
+    try {
+        const { movieDelete } = await Movie.destroy({ where: { id } })
+        res.status(200).json({ message: 'Movie deleted' })
+    } catch (error) {}
 }
--- server/routes/movies.routes.js
+++ server/routes/movies.routes.js
@@ -1,8 +1,17 @@
 import { Router } from 'express'
-import { createMovie, getMovies } from '../controllers/movie.controller.js'
+import {
+    createMovie,
+    deleteMovie,
+    editMovie,
+    getMovie,
+    getMovies
+} from '../controllers/movie.controller.js'
 const router = Router()
 
 router.get('/movie', getMovies)
 router.post('/movie', createMovie)
+router.put('/movie/:id', editMovie)
+router.get('/movie/:id', getMovie)
+router.delete('/movie/:id', deleteMovie)
 
 export default router
