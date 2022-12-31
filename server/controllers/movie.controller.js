ba7305abbcaa90ee9a84838557b43b2a959a854b
AleKusa <alekusa@gmail.com>
2022-12-30 21:56:02 -0300

    Movie Get_ All

--- server/controllers/movie.controller.js
+++ server/controllers/movie.controller.js
@@ -4,10 +4,19 @@ export const createMovie = async (req, res) => {
     try {
         //await Movie.sync({ force: true })
         const { picture, title, calification } = req.body
-        const newMovie = new Movie({ picture, title, calification })
+        const newMovie = Movie.build({ picture, title, calification })
         await newMovie.save()
         return res.send(newMovie)
     } catch (error) {
         return res.status(500).json({ message: error.message })
+    }
+}
+
+export const getMovies = async (req, res) => {
+    try {
+        const movies = await Movie.findAll()
+        res.send(movies)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }
--- server/routes/movies.routes.js
+++ server/routes/movies.routes.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { createMovie } from '../controllers/movie.controller.js'
+import { createMovie, getMovies } from '../controllers/movie.controller.js'
 const router = Router()
 
+router.get('/movie', getMovies)
 router.post('/movie', createMovie)
 
 export default router
