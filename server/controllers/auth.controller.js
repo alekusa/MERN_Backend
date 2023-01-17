27b9e62e6099eaba545a8ca8035d0bdd3dbe224c
alekusa <alekusa@gmail.com>
2023-01-17 12:28:34 -0300

    Add auth.service - separating the functions of controller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,57 +1,56 @@
 // Modelos a utilizar en la creacion de usuarios
-import User from '../models/user.model.js'
-import Role from '../models/role.model.js'
+//import User from '../models/user.model.js'
+//import Role from '../models/role.model.js'
 // para generar un token
-import jwt from 'jsonwebtoken'
+//import jwt from 'jsonwebtoken'
 // class donde estan las funciones para encriptar las password
-import servCript from '../services/cript.js'
-const serv = new servCript()
+//import servCript from '../services/cript.js'
+//const serv = new servCript()
+//* Cargando auth.service.js
+import authService from '../services/auth.service.js'
+const autServ = new authService()
 
 export const signUp = async (req, res) => {
-    const { username, email, password, role } = req.body
-    const encriptada = await serv.encryptPassword(password)
-
-    const newUser = User.build({
-        username,
-        email,
-        password: encriptada
-    })
-    console.log(role)
-    if (role) {
-        //* si encuentra el nombre pasado por req.body.role en la tabla Role, asigna su id a roleId,
-        //* si mandan cualquier role que no esta en la tabla se asigna el 1 que es Users
-        const roleId = await Role.findOne({ where: { name: role } })
-        if (roleId) {
-            newUser.role = roleId.id
-            await newUser.save()
-        } else {
-            newUser.role = 1
-            await newUser.save()
-        }
-    } else {
-        //* si no se asigna nada tambien le asignamos el 1 que es Users
-        newUser.role = 1
-        await newUser.save()
+    try {
+        res.status(200).json(await autServ.newUsers(req.body))
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
-    const token = jwt.sign({ id: newUser.id }, 'palabraSecreta', {
-        expiresIn: 86400 //*24hs
-    })
-    res.json({ token, newUser })
+    // const { username, email, password, role } = req.body
+    // const encriptada = await serv.encryptPassword(password)
+
+    // const newUser = User.build({
+    //     username,
+    //     email,
+    //     password: encriptada
+    // })
+    // if (role) {
+    //     //* si encuentra el nombre pasado por req.body.role en la tabla Role, asigna su id a roleId,
+    //     //* si mandan cualquier role que no esta en la tabla se asigna el 1 que es Users
+    //     const roleId = await Role.findOne({ where: { name: role } })
+    //     if (roleId) {
+    //         newUser.role = roleId.id
+    //         await newUser.save()
+    //     } else {
+    //         newUser.role = 1
+    //         await newUser.save()
+    //     }
+    // } else {
+    //     //* si no se asigna nada tambien le asignamos el 1 que es Users
+    //     newUser.role = 1
+    //     await newUser.save()
+    // }
+    // const token = jwt.sign({ id: newUser.id }, 'palabraSecreta', {
+    //     expiresIn: 86400 //*24hs
+    // })
+    // res.json({ token, newUser })
 }
 export const signIn = async (req, res) => {
-    const user = await User.findOne({
-        where: { email: req.body.email }
-    })
-    //console.log(user.username)
-    if (!user) return res.status(400).json({ message: 'User not fund' })
-
-    const mathpassword = await serv.comparePassword(
-        req.body.password,
-        user.password
-    )
-    if (!mathpassword) return res.status(401).json({ token: null })
-    const token = jwt.sign({ id: user.id }, 'palabraSecreta', {
-        expiresIn: 86400
-    })
-    res.json({ token: token })
+    try {
+        res.status(200).json(
+            await autServ.mathpassword(req.body.password, req.body.email)
+        )
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
---
+++ server/services/auth.service.js
@@ -0,0 +1,57 @@
+import User from '../models/user.model.js'
+import Role from '../models/role.model.js'
+import servCript from './cript.js'
+const serv = new servCript()
+
+import jwt from 'jsonwebtoken'
+
+class authService {
+    async mathpassword(bodyPass, bodyEmail) {
+        const user = await User.findOne({
+            where: { email: bodyEmail }
+        })
+        const compare = await serv.comparePassword(bodyPass, user.password)
+        if (compare) {
+            const token = jwt.sign({ id: user.id }, 'palabraSecreta', {
+                expiresIn: 86400
+            })
+            return { token }
+        } else {
+            return { Password: 'Password Incorrect', token: 'Null' }
+        }
+    }
+    //! function para crear usuarios nuevos !
+    async newUsers(object) {
+        const { username, email, password, role } = object
+        const existEmail = await User.findOne({ where: { email } })
+        if (!existEmail) {
+            const passCrypt = await serv.encryptPassword(password)
+            const newUser = User.build({
+                username,
+                email,
+                password: passCrypt
+            })
+            if (role) {
+                const idRole = await Role.findOne({ where: { name: role } })
+                console.log(idRole.id)
+                if (idRole) {
+                    newUser.role = idRole.id
+                    await newUser.save()
+                } else {
+                    newUser.role = 1
+                    newUser.save()
+                }
+            } else {
+                newUser.role = 1
+                newUser.save()
+            }
+            const token = jwt.sign({ id: newUser.id }, 'palabraSecreta', {
+                expiresIn: 86400 //*24hs
+            })
+            return { token }
+        } else {
+            return { Email: 'this email already exists' }
+        }
+    }
+}
+export default authService
